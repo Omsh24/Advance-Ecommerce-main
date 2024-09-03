af576fbcea254a7f5787ef50d8b6a3cd3a5b3d33
Rachit Jain <rachitjainjaipur@gmail.com>
2024-09-04 00:09:36 +0530

    signup page done

--- Server/src/controller/user.controller.js
+++ Server/src/controller/user.controller.js
@@ -1,10 +1,11 @@
 import { User } from "../models/user.model.js"
 import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 
 
-const generateAccessAndRefreshToken = async(userId) => {
+const generateAccessAndRefreshToken = async (userId) => {
     try {
         const user = await User.findById(userId);
 
@@ -12,14 +13,47 @@ const generateAccessAndRefreshToken = async(userId) => {
         const refreshToken = user.generateRefreshToken()
 
         user.refreshToken = refreshToken;
-        await user.save({validateBeforeSave: false})
+        await user.save({ validateBeforeSave: false })
 
-        return {accessToken, refreshToken}
+        return { accessToken, refreshToken }
     } catch (error) {
         throw new ApiError(500, "Something went wrong while generating refresh and access token")
     }
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-    const {firstName, lastName, password, email, phoneNumber, address, role} = req.body;
+    const { firstName, lastName, password, email, phoneNumber, address, role } = req.body;
+
+    if (!firstName || !lastName || !password || !email || !phoneNumber || !address) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    if (!email.includes('@')) {
+        throw new ApiError(400, "Invalid Email address");
+    }
+
+    const existedUser = await User.findOne(email)
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    const user = await User.create({
+        firstName: firstName,
+        password: password,
+        email: email,
+        lastName: lastName,
+        address: address,
+        phoneNumber: phoneNumber,
+        role: role
+    })
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering a user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, createdUser, "User registered Successfully")
+    );
+
 })
